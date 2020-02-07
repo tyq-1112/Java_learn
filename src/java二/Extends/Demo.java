fdfaadd48b45d5a2366526c9aa719b243d5dc3e8
FairyLd <2950498345@qq.com>
2020-02-07 12:00:00 +0800

    继承

--- src/java二/Extends/Demo.java
+++ src/java二/Extends/Demo.java
@@ -18,7 +18,7 @@ class Student extends People{             //子类
     }
 }
 
-public class Demo {
+public class Demo  {
     public static void main(String[] args) {
         new Student("小仙女",20,"清华大学");
         //实例化子类对象的同时一定会实例化父类对象
